5efd47fdd571c8a1e44532862162345dac36a7e6
Paul Bellamy <paul.a.bellamy@gmail.com>
2015-01-10 15:39:56 +0000

    adding some basic comments

--- file_system.go
+++ file_system.go
@@ -8,6 +8,8 @@ import (
 	"strings"
 )
 
+// Ensure that the filesystem is loaded. If loading the filesystem fails, this
+// will panic.
 func Must(fs http.FileSystem, err error) http.FileSystem {
 	if err != nil {
 		panic(err)
@@ -15,6 +17,8 @@ func Must(fs http.FileSystem, err error) http.FileSystem {
 	return fs
 }
 
+// Load the current executable as an archive. If you've appended your zip file
+// of assets to your executable, this is how you should retrieve them.
 func CurrentExecutable(f Format) (http.FileSystem, error) {
 	path, err := exec.LookPath(os.Args[0])
 	if err != nil {
@@ -23,6 +27,7 @@ func CurrentExecutable(f Format) (http.FileSystem, error) {
 	return FromFile(f, path)
 }
 
+// Load an archive from a file path.
 func FromFile(f Format, path string) (http.FileSystem, error) {
 	file, err := os.Open(path)
 	if err != nil {
@@ -33,11 +38,12 @@ func FromFile(f Format, path string) (http.FileSystem, error) {
 	return FromReader(f, file)
 }
 
-// Read the tar contents and rebuild the file system
+// Load an archive from a string of binary data.
 func FromString(f Format, content string) (http.FileSystem, error) {
 	return FromReader(f, strings.NewReader(content))
 }
 
+// Load an archive from an io.Reader.
 func FromReader(f Format, r io.Reader) (http.FileSystem, error) {
 	return f.FromReader(r)
 }
